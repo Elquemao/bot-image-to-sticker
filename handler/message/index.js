7eee48bd20e3bc92c9bf1e49c5fb786fe961321d
Alejandro <alejandro.ramon.palarea@gmail.com>
2021-07-17 00:15:50 +0200

    New functions added, newcommands like #casa, #audio were added too. Fixed bugs and better logging.

--- handler/message/index.js
+++ handler/message/index.js
@@ -2,11 +2,11 @@ require('dotenv').config()
 const { decryptMedia, Client } = require('@open-wa/wa-automate')
 const moment = require('moment-timezone')
 moment.tz.setDefault('Europe/Madrid').locale('id')
-const {cekResi, urlShortener, meme, translate, getLocationData } = require('../../lib')
+const {meme, translate, getLocationData } = require('../../lib')
 const { msgFilter, color, processTime, is } = require('../../utils')
 const mentionList = require('../../utils/mention')
 const { uploadImages } = require('../../utils/fetcher')
-
+const fs = require('fs')
 const { menuEs, menuEn } = require('./text') // Spanish & English menu
 
 module.exports = msgHandler = async (client, message) => {
@@ -22,8 +22,8 @@ module.exports = msgHandler = async (client, message) => {
         const groupMembers = isGroupMsg ? await client.getGroupMembersId(groupId) : ''
         const isGroupAdmins = groupAdmins.includes(sender.id) || false
         const isBotGroupAdmins = groupAdmins.includes(botNumber) || false
-        const bodyWithoutFilter = body.toLowerCase()
-        
+        const bodyWithoutFilter = (type === 'chat') ? body.toLowerCase() : ''
+
         // Bot Prefix
         const prefix = '#'
         body = (type === 'chat' && body.startsWith(prefix)) ? body : (((type === 'image' || type === 'video') && caption) && caption.startsWith(prefix)) ? caption : ''
@@ -34,7 +34,7 @@ module.exports = msgHandler = async (client, message) => {
         const isQuotedImage = quotedMsg && quotedMsg.type === 'image'
         const url = args.length !== 0 ? args[0] : ''
         const uaOverride = process.env.UserAgent
-        // [BETA] Avoid Spam Message (5s of delay)
+        // Avoid Spam Message (5s of delay)
         if (isCmd && msgFilter.isFiltered(from) && !isGroupMsg) { 
             return console.log(color('[SPAM]', 'red'), color(moment(t * 1000).format('DD/MM/YYYY HH:mm:ss'), 'yellow'), color(`${command} [${args.length}]`), 'from', color(pushname)) 
         }
@@ -47,9 +47,15 @@ module.exports = msgHandler = async (client, message) => {
             [RECV] --> Received messages
             [EXEC] --> Executed commands from server
         */
-        if (bodyWithoutFilter == 'piti') { 
-            return client.sendText(from, 'Pitilloooooo maricoooooooooooooooon') 
+        if (bodyWithoutFilter.includes('piti') || bodyWithoutFilter.includes('pitillo') || bodyWithoutFilter.includes('antonio')) { 
+            let random = Math.floor(Math.random() * 6)
+            console.log(color('[EXEC]'), color(moment(t * 1000).format('DD/MM/YYYY HH:mm:ss'), 'yellow'), color('Message send it', color), 'from', color(pushname))
+            return client.reply(from, menuEs.insults(random), id)
         }
+        if (bodyWithoutFilter.includes('alfon') || bodyWithoutFilter.includes('alfonso')) { 
+            console.log(color('[EXEC]'), color(moment(t * 1000).format('DD/MM/YYYY HH:mm:ss'), 'yellow'), color('Message send it'), 'from', color(pushname))
+            return client.reply(from, 'Alonso rapido y furioso', id)
+        } 
         if (!isCmd && !isGroupMsg) { 
             return console.log('[RECV]', color(moment(t * 1000).format('DD/MM/YYYY HH:mm:ss'), 'yellow'), 'Message from', color(pushname)) 
         }
@@ -65,11 +71,20 @@ module.exports = msgHandler = async (client, message) => {
         switch (command) {
         // Menu
         case 'ping':
-            await client.sendText(from, `Ping: ${processTime(t, moment())} s`)
+            await client.sendText(from, `Ping: ${processTime(t, moment())}s`)
             break
         case 'menu':
         case 'help':
             await client.sendText(from, menuEs.textMenu(pushname))
+            break
+        case 'verdad':
+            await client.sendText(from, menuEs.verdad())
+            break
+        case 'copon':
+            await client.sendText(from, menuEs.copon())
+            break
+        case 'casa':
+            await client.sendLocation(from, 38.993441, -1.853568, 'Bindi Albacete')
             break
         // Sticker Creator
         case 'sticker': {
@@ -79,14 +94,14 @@ module.exports = msgHandler = async (client, message) => {
                 const mediaData = await decryptMedia(encryptMedia, uaOverride)
                 const imageBase64 = `data:${_mimetype};base64,${mediaData.toString('base64')}`
                 client.sendImageAsSticker(from, imageBase64, {keepScale: true}).then(() => {
-                    client.reply(from, 'Pa\' ti', id)
+                    client.reply(from, 'Pa\' ti hijo de la gran puta', id)
                     console.log(`Sticker Processed in ${processTime(t, moment())}s`)
                 })
             } else if (args.length === 1) {
                 if (!is.Url(url)) { await client.reply(from, 'Envía un enlace válido anda', id) }
                 client.sendStickerfromUrl(from, url).then((r) => (!r && r !== undefined)
                     ? client.sendText(from, 'El enlace no tiene ninguna imagen')
-                    : client.reply(from, 'Pa\' ti', id)).then(() => console.log(`Sticker Processed in ${processTime(t, moment())}s`))
+                    : client.reply(from, 'Pa\' ti hijo de la gran puta', id)).then(() => console.log(`Sticker Processed in ${processTime(t, moment())}s`))
             } else {
                 await client.reply(from, 'Escribe bien el comando', id)
             }
@@ -132,6 +147,47 @@ module.exports = msgHandler = async (client, message) => {
             client.sendText(from, `Estadísiticas:\n- *${loadedMsg}* Mensajes leídos\n- *${groups.length}* Grupos\n- *${chatIds.length - groups.length}* Individuales\n- *${chatIds.length}* Chats totales`)
             break
         }
+        case 'audio':
+            if(args.length != 1) {
+                return client.reply(from, 'Escribe bien el comando', id)
+            }
+
+            let audioPath = 'handler\\message\\audio\\' + args[0].toLowerCase() + '.ogg'
+
+            fs.access(audioPath, fs.F_OK, (err) => {
+                if(err) {
+                    console.log(color('[ERROR]', 'red'), color(moment(t * 1000).format('DD/MM/YYYY HH:mm:ss'), 'yellow'), color(`File ${args[0]} not found`, 'red'), 'from', color(pushname, 'red')) 
+                    return client.reply(from, 'Ese audio no existe', id)
+                }
+
+                client.sendAudio(from, audioPath, id)
+                .then(() => {
+                    console.log(`Audio Processed in ${processTime(t, moment())} s`)
+                })
+                .catch((err) => console.log(err))
+            })
+            break
+        //ON DEVELOPMENT
+        /*case 'meme':
+            if ((isMedia || isQuotedImage) && args.length >= 2) {
+                const top = arg.split('|')[0]
+                const bottom = arg.split('|')[1]
+                const encryptMedia = isQuotedImage ? quotedMsg : message
+                const mediaData = await decryptMedia(encryptMedia, uaOverride)
+                const getUrl = await uploadImages(mediaData, false)
+                const ImageBase64 = await meme.custom(getUrl, top, bottom)
+                client.sendFile(from, ImageBase64, 'image.png', '', null, true)
+                    .then((serialized) => console.log(`Success sending file with id: ${serialized}, processing time was ${processTime(t, moment())}`))
+                    .catch((err) => console.error(err))
+            } else if (arg.split(" ")[0] == 'random') {
+                const ImageBase64 = meme.random(arg.split(" ")[1])
+                client.sendFile(from, ImageBase64, 'image.png', '', null, true)
+                    .then((serialized) => console.log(`Success sending file with id: ${serialized}, processing time was ${processTime(t, moment())}`))
+                    .catch((err) => console.error(err))
+            } else {
+                await client.reply(from, 'Introduce bien el comando!! \nSi tienes dudas pon el comando #help', id)
+            }
+            break*/
         default:
             console.log(color('[ERROR]', 'red'), color(moment(t * 1000).format('DD/MM/YYYY HH:mm:ss'), 'yellow'), 'Unregistered Command from', color(pushname))
             break
--- handler/message/text/es.js
+++ handler/message/text/es.js
@@ -13,6 +13,47 @@ Envía el comando #gifsticker seguido del gif.
 
 3. *#translate* _<Idioma>_
 Traduce el mensaje que quieras al idioma que quieras.
+*Deprecated*
+
+4. *#verdad*
+Manda tocho sobre lo que no es verdad
+
+5. *#copon*
+Manda tocho del copón
+
+4. *#audio* _<nombre>_
+Envía el comando #audio seguido del nombre del audio que quieras que te devuelva el bot.
+Por ejemplo: #audio hawelin
+
+Audios disponibles:
+- hawelin
+- juli
+- kevin
+- pablo
+- tururu
 
 Se irá mejorando poco a poco!✨`
+}
+
+exports.verdad = () => {
+    return `
+    Sabes perfectamente que eso no es verdad. Y no contento con ello vienes aquí, al grupo, a crear crispación. Solo para regodearte de las respuestas que obtengas, evidentemente en contra de tu posicionamiento que sabes erróneo.
+    No, esto no es gracioso. Tu falta de sensibilidad hacia este tema hace que mucha gente se sienta mal. De acuerdo que eso es un grupo, pero hay cosas que pueden traspasar la pantalla del ordenador y herir el orgullo de quien te lea. Tú has pasado una barrera que no debías y por eso me causas poco menos que repugnancia. 
+    Voy a respetarte a nivel personal, porque sé que en definitiva, solo tratas de divertirte a costa ajena, aunque con dudoso gusto. Por otra parte, te agradecería de que dejaras de considerarte amigo mío. Un saludo, espero que recapacites y no vuelvas a hacerlo.`
+}
+
+exports.copon = () => {
+    return `
+    Menudo hijo de puta, tienes un cuerpazo del copón. De moreno nada, tu estás blanco. Menudas espaldas tienes, hijo de la gran puta. Olvídate de adelgazar, potencia ese físico tan espectacular que tienes, hijo de una perra sarnosa. Qué puta envidia me das! Cuida un poco el sol y tira solarium en el gim. sol y zanahoria es lo que necesitas para terminar de ponerte del copón. Menudo hijo de la gran puta eres, ya quiesiera yo ese pedazo de cuerpo. Cabronazo, hijo de mil putas, te invidio muchísimo. Insisto, cuida un poco el sol y machácate en el gim, con ese cuerpo puedes quedarte del copón, cabronazo.
+Y a los que dicen que estás blanco ni puto caso, son un atajo de maricones moreno-palos que solo les gusta tomar el sol Tu, sin embargo, estás del copón, hijo de la grandísima puta. Qué suerte tenéis algunos con la genética.`
+}
+
+exports.insults = (random) => {
+    let insultos = ['Pitillo chupa pija', 
+    'Pitilloooooooooooo maricoooooooon', 
+    'Pitillo es un subnormal', 
+    'Pitillo, eres un puto pringado', 
+    'Pitillo cara alfombra', 
+    'Pitillo, eres un tobogán de piojos']
+    return insultos[random]
 }
